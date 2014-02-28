ab2d6d96839ff7720d22026070bdb2b0363ef30c
Christoph Waldhauser <chw@kdss.at>
2014-02-28 00:00:00 +0000

    version 0.3-5

---
+++ R/dictionary.R
@@ -0,0 +1,11 @@
+### Hard-coded Google+ API URLs
+
+base.url <- "https://www.googleapis.com/plus/v1/"
+
+start.people <- "people/"
+close.page <- "/activities/public?maxResults=100&key="
+
+close.people <- "?key="
+
+gp <- new.env(parent=emptyenv())
+assign("apikey", NULL, envir=gp)
